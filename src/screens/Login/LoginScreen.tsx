2ebf3c8dc873e561652a27de51288140908b4a05
guadalupe <guadalupe.alavila@gmail.com>
2023-07-20 18:55:03 -0300

    redirect to signUp

--- src/screens/Login/LoginScreen.tsx
+++ src/screens/Login/LoginScreen.tsx
@@ -5,20 +5,24 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackLogoutParamList } from '../../navigations/types';
 import useAuth from '../../shared/hooks/useAuth';
 import Loading from '../../shared/components/Loading';
+import Button from '../../shared/components/Button';
 
 interface Props extends NativeStackScreenProps<RootStackLogoutParamList, 'LoginScreen'> {}
 
-const LoginScreen = ({}: Props) => {
+const LoginScreen = ({ navigation }: Props) => {
     const { isLoading, loginWithEmailAndPass } = useAuth();
 
     return (
         <Container>
             {!isLoading ? (
-                <LoginForm
-                    onSubmit={({ email, password }) => {
-                        loginWithEmailAndPass(email.trim().toLowerCase(), password.trim().toLowerCase());
-                    }}
-                />
+                <>
+                    <LoginForm
+                        onSubmit={({ email, password }) => {
+                            loginWithEmailAndPass(email.trim().toLowerCase(), password.trim().toLowerCase());
+                        }}
+                    />
+                    <Button outlined title='Registrarse' onPress={() => navigation.navigate('SignUpScreen')} />
+                </>
             ) : (
                 <Loading />
             )}
