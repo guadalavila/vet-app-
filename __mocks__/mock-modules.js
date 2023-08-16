24e5c7175be88338a3b1636bd7dd1731c589015a
guadalupe <guadalupe.alavila@gmail.com>
2023-08-16 00:41:01 -0300

    setup testing-library for test

---
+++ __mocks__/mock-modules.js
@@ -0,0 +1,20 @@
+const mockedNavigation = jest.fn();
+const mockedGoBack = jest.fn();
+
+jest.mock('@react-navigation/native', () => {
+    return {
+        useNavigation: () => ({
+            navigate: mockedNavigation,
+            goBack: mockedGoBack,
+        }),
+    };
+});
+
+jest.mock('react-native-safe-area-context', () => {
+    const inset = { top: 0, right: 0, bottom: 0, left: 0 };
+    return {
+        SafeAreaProvider: jest.fn().mockImplementation(({ children }) => children),
+        SafeAreaConsumer: jest.fn().mockImplementation(({ children }) => children(inset)),
+        useSafeAreaInsets: jest.fn().mockImplementation(() => inset),
+    };
+});
